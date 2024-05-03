56bdb4d80f71cdf282f51bbf2d65e68f9f37dd2b
nazaquino98 <nazarenaayelen49@gmail.com>
2024-05-03 10:32:43 -0300

    se termino el segundo punto

--- assets/js/parImpar.js
+++ assets/js/parImpar.js
@@ -1,1 +1,10 @@
-let numero
+let numero = parseInt(prompt("Ingresa un número:"));
+
+if (numero % 2 === 0) {
+ console.log("El numero es par");
+    } else if (numero % 2 === 1)  {
+    console.log("El numero es impar");
+} 
+else {
+    console.log("Error. Ingresa un numero valido");
+}
