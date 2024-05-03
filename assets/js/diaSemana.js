2452b325ed095cf411919a363d9180100949b207
nazaquino98 <nazarenaayelen49@gmail.com>
2024-05-03 11:21:20 -0300

    se termino el punto diaSemana

--- assets/js/diaSemana.js
+++ assets/js/diaSemana.js
@@ -1,1 +1,39 @@
-let numero
+
+let numero = prompt("Ingresa un número del 1 al 7:");
+numero=parseInt(numero);
+switch (numero) {
+    case 1: 
+    console.log("El dia de la semana que correspoonde al numero ingresado es lunes")
+    
+        break;
+
+    case 2: 
+    console.log("El dia de la semana que correspoonde al numero ingresado es martes")
+        break;
+
+    case 3:
+        console.log("El dia de la semana que correspoonde al numero ingresado es miercoles")
+    break;
+
+    case 4:
+        console.log("El dia de la semana que correspoonde al numero ingresado es jueves")
+    break;
+
+    case 5: 
+    console.log("El dia de la semana que correspoonde al numero ingresado es viernes")
+    break;
+
+    case 6:
+        console.log("El dia de la semana que correspoonde al numero ingresado es sabado")
+    break;
+
+    case 7:
+        console.log("El dia de la semana que correspoonde al numero ingresado es domingo")
+    break;
+
+    default: console.log("Error. Debe ser un numero del 1 al 7.");
+}
+
+
+
+
