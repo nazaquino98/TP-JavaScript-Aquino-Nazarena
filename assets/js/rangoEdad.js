213eddf2ed2ac9f3205b10d6b1aac8c6a0dd2e4c
nazaquino98 <nazarenaayelen49@gmail.com>
2024-05-03 10:00:19 -0300

    se crearon las condiciones para rangoEdad

--- assets/js/rangoEdad.js
+++ assets/js/rangoEdad.js
@@ -1,1 +1,17 @@
-let edad
+let edad = prompt ("Ingresa tu edad");
+
+if (edad >= 0 && edad <= 12) {
+    console.log("Sos un niño.");
+
+} else if (edad >= 13 && edad <= 19) {
+    console.log("Sos un adolescente.");
+
+  } else if (edad >= 20 && edad <= 59) {
+    console.log("Sos un adulto.");
+
+  } else if (edad >= 60) {
+    console.log("Sos un adulto mayor.");
+
+  } else {
+    console.log("Error. No es una edad. Ingresa una edad valida");
+  }
